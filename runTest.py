02c67376867dfa635b23f70d36a0d0fc4e8c3df0
Jeremy-Thorne <Jeremy@genesisdigital.asia>
2023-04-17 18:52:13 +1000

    More Data

--- runTest.py
+++ runTest.py
@@ -2,25 +2,24 @@ import subprocess
 import random
 
 dataList = []
-total = 0
-count = 0
 path = "C:/Users/Jeremy/Documents/Assignment1-Algo/Assignment1-Algo/"
 sizeList = [100,250,500,1000,2500,5000,10000,25000,50000,100000]
 densityList = [0.05, 0.5, 0.95]
 timesList = []
 typeList = ["array"]
 
-def createFiles(density):
-    for size in sizeList:
-        with open('C:/Users/Jeremy/Documents/Assignment1-Algo/Assignment1-Algo/'+ str(size) + '.txt', 'w') as f:
+def createFiles():
+    for density in densityList:
+        for size in sizeList:
+            f = open('C:/Users/Jeremy/Documents/Assignment1-Algo/Assignment1-Algo/Data/'+ str(size) + "-" + str(density) + '.txt', 'w')
             for i in range(size):
-                for j in range(10):
-                    num = round(random.random(), 2)
-                    if num > density:                        
-                        break
-                    else:
-                        string = str(int(random.random() * size)) + " " + str(int(random.random() * 10)) + " " + str(round(random.random(), 2)) + "\n"
-                        f.write(string)
+                num = round(random.random(), 2)
+                if num > density:                        
+                    pass
+                else:
+                    string = str(int(random.random() * size)) + " " + str(int(random.random() * 10)) + " " + str(round(random.random(), 2)) + "\n"
+                    f.write(string)
+            f.close()
 def runTest():
     try:
         stdout = subprocess.run(["python", path + "spreadsheetFilebasedTesting.py", 
@@ -33,15 +32,16 @@ def runTest():
     print(stdout)
 
 if __name__ == '__main__':
-    #createFiles(0.2)
+    # createFiles()
     # runTest()
     # exit()
     for l in range(len(typeList)):
         for k in range(len(densityList)):
-            createFiles(densityList[k])
             timesList.append("\n" + 'array - density -'+ str(densityList[k]) + ":")
             for i in range(len(sizeList)):
-                dataString = path + str(sizeList[i]) + ".txt"
+                total = 0
+                count = 0
+                dataString = path + "Data/" + str(str(sizeList[i])) + "-" + str(densityList[k]) + '.txt'
                 print(sizeList[i], end=" ")
                 for j in range(5):
                     try:
@@ -62,8 +62,9 @@ if __name__ == '__main__':
                 timesList.append("Time taken for data size - " + str(sizeList[i]) + ": " + str(avg))
             print()
 
-        with open(path + 'time.txt', 'a') as f:
+        with open(path + 'time.txt', 'w') as f:
             for time in timesList:
                 f.write(time + "\n")
+            f.close()
 
 
